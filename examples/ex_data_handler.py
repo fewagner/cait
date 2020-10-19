781497ec819013124409b883d6955c1587b79f2b
Damir Rizvanovic <damir.rizvanovic@oeaw.ac.at>
2020-10-19 18:46:40 +0200

    Revert "Path /run35_ML already defined in DataHandler.set_filepath()"
    This reverts commit 01d2bdccc4f0b2ba9fabf1216697102c7ce6e19c

--- examples/ex_data_handler.py
+++ examples/ex_data_handler.py
@@ -1,8 +1,8 @@
-import cait as ai
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+import cait as ai
 
 if __name__ == '__main__':
     # create Instance of the DataHandler Class
@@ -12,9 +12,9 @@ if __name__ == '__main__':
                         record_length=16384)
 
     # the instance can convert the rdt file to a hdf5 file
-    dh.convert_dataset(path_rdt='toy_data/',
+    dh.convert_dataset(path_rdt='toy_data/run35_DetF/',
                        fname='bck_001',
-                       path_h5='toy_data/',
+                       path_h5='toy_data/run35_DetF/',
                        tpa_list=[0.],
                        calc_mp=True,
                        calc_fit=False,
