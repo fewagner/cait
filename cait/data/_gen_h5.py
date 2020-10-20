00bd844284d065033cd42a0064dcd181938b26d6
dRizvanovic <damir.rizvanovic@oeaw.ac.at>
2020-10-20 19:00:49 +0200

    Fixed tpa_list-bug for testpulse-data

--- cait/data/_gen_h5.py
+++ cait/data/_gen_h5.py
@@ -67,9 +67,12 @@ def gen_dataset_from_rdt(path_rdt,
 
             print('CALCULATE MAIN PARAMETERS.')
 
-            with get_context("spawn").Pool(processes) as p:  # basically a for loop running on 4 processes
-                p_mainpar_list_event = p.map(calc_main_parameters, pulse_event[0, :, :])
-                l_mainpar_list_event = p.map(calc_main_parameters, pulse_event[1, :, :])
+            # basically a for loop running on 4 processes
+            with get_context("spawn").Pool(processes) as p:
+                p_mainpar_list_event = p.map(
+                    calc_main_parameters, pulse_event[0, :, :])
+                l_mainpar_list_event = p.map(
+                    calc_main_parameters, pulse_event[1, :, :])
             mainpar_event = np.array([[o.getArray() for o in p_mainpar_list_event],
                                       [o.getArray() for o in l_mainpar_list_event]])
 
@@ -78,14 +81,15 @@ def gen_dataset_from_rdt(path_rdt,
             print('CALCULATE FIT.')
 
             with get_context("spawn").Pool(processes) as p:
-                p_fitpar_event = np.array(p.map(fit_pulse_shape, pulse_event[0, :, :]))
-                l_fitpar_event = np.array(p.map(fit_pulse_shape, pulse_event[1, :, :]))
+                p_fitpar_event = np.array(
+                    p.map(fit_pulse_shape, pulse_event[0, :, :]))
+                l_fitpar_event = np.array(
+                    p.map(fit_pulse_shape, pulse_event[1, :, :]))
 
             fitpar_event = np.array([p_fitpar_event, l_fitpar_event])
         # else:
         #     p_fitpar_event = np.zeros([len(pulse_event[0]), 6])
         #     l_fitpar_event = np.zeros([len(pulse_event[1]), 6])
-
 
         print('CREATE DATASET WITH EVENTS.')
 
@@ -121,20 +125,30 @@ def gen_dataset_from_rdt(path_rdt,
             # ################# STD EVENT #################
             # [pulse_height, t_zero, t_rise, t_max, t_decaystart, t_half, t_end, offset, linear_drift, quadratic_drift]
             p_stdevent_pulse, p_stdevent_fitpar = generate_standard_event(pulse_event[0, :, :],
-                                                                          mainpar_event[0, :, :],
-                                                                          pulse_height_intervall=[0.05, 1.5],
+                                                                          mainpar_event[0,
+                                                                                        :, :],
+                                                                          pulse_height_intervall=[
+                                                                              0.05, 1.5],
                                                                           left_right_cutoff=0.1,
-                                                                          rise_time_intervall=[5, 100],
-                                                                          decay_time_intervall=[50, 2500],
-                                                                          onset_intervall=[1500, 3000],
+                                                                          rise_time_intervall=[
+                                                                              5, 100],
+                                                                          decay_time_intervall=[
+                                                                              50, 2500],
+                                                                          onset_intervall=[
+                                                                              1500, 3000],
                                                                           verb=True)
             l_stdevent_pulse, l_stdevent_fitpar = generate_standard_event(pulse_event[1, :, :],
-                                                                          mainpar_event[1, :, :],
-                                                                          pulse_height_intervall=[0.05, 1.5],
+                                                                          mainpar_event[1,
+                                                                                        :, :],
+                                                                          pulse_height_intervall=[
+                                                                              0.05, 1.5],
                                                                           left_right_cutoff=0.1,
-                                                                          rise_time_intervall=[5, 100],
-                                                                          decay_time_intervall=[50, 2500],
-                                                                          onset_intervall=[1500, 3000],
+                                                                          rise_time_intervall=[
+                                                                              5, 100],
+                                                                          decay_time_intervall=[
+                                                                              50, 2500],
+                                                                          onset_intervall=[
+                                                                              1500, 3000],
                                                                           verb=True)
 
             stdevent = h5f.create_group('stdevent')
@@ -178,8 +192,10 @@ def gen_dataset_from_rdt(path_rdt,
         noise.create_dataset('event', data=np.array(pulse_noise))
         noise.create_dataset('hours', data=np.array(metainfo_noise[0, :, 10]))
 
-        p_mean_nps, _ = calculate_mean_nps(pulse_noise[0, :, :], record_length=len(pulse_noise[0, 0]))
-        l_mean_nps, _ = calculate_mean_nps(pulse_noise[1, :, :], record_length=len(pulse_noise[0, 0]))
+        p_mean_nps, _ = calculate_mean_nps(
+            pulse_noise[0, :, :], record_length=len(pulse_noise[0, 0]))
+        l_mean_nps, _ = calculate_mean_nps(
+            pulse_noise[1, :, :], record_length=len(pulse_noise[0, 0]))
         noise.create_dataset('nps', data=np.array([p_mean_nps, l_mean_nps]))
 
     if (-1.0 in tpa_list) and (0 in tpa_list) and calc_sev:
@@ -193,9 +209,10 @@ def gen_dataset_from_rdt(path_rdt,
 
     # ################# PROCESS TESTPULSES #################
     # if we filtered for testpulses
-    if np.logical_and(tpa_list != -1.0, tpa_list != 0.0).any():
+    if any(el > 0 for el in tpa_list):
         print('WORKING ON EVENTS WITH TPA > 0.')
-        tp_list = np.logical_and(metainfo[1, :, 12] != -1.0, metainfo[1, :, 12] != 0.0)
+        tp_list = np.logical_and(
+            metainfo[1, :, 12] != -1.0, metainfo[1, :, 12] != 0.0)
 
         metainfo_tp = metainfo[:, tp_list, :]
         pulse_tp = pulse[:, tp_list, :]
@@ -203,33 +220,39 @@ def gen_dataset_from_rdt(path_rdt,
         if calc_mp:
             print('CALCULATE MP.')
 
-            with get_context("spawn").Pool(processes) as p:  # basically a for loop running on 4 processes
-                p_mainpar_list_tp = p.map(calc_main_parameters, pulse_tp[0, :, :])
+            # basically a for loop running on 4 processes
+            with get_context("spawn").Pool(processes) as p:
+                p_mainpar_list_tp = p.map(
+                    calc_main_parameters, pulse_tp[0, :, :])
             p_mainpar_tp = np.array([o.getArray() for o in p_mainpar_list_tp])
 
             with get_context("spawn").Pool(processes) as p:
-                l_mainpar_list_tp = p.map(calc_main_parameters, pulse_tp[1, :, :])
+                l_mainpar_list_tp = p.map(
+                    calc_main_parameters, pulse_tp[1, :, :])
             l_mainpar_tp = np.array([o.getArray() for o in l_mainpar_list_tp])
 
             mainpar_tp = np.array([p_mainpar_tp, l_mainpar_tp])
-
 
         if calc_fit:
             print('CALCULATE FIT.')
 
             with get_context("spawn").Pool(processes) as p:
-                p_fitpar_tp = np.array(p.map(fit_pulse_shape, pulse_tp[0, :, :]))
+                p_fitpar_tp = np.array(
+                    p.map(fit_pulse_shape, pulse_tp[0, :, :]))
 
             with get_context("spawn").Pool(processes) as p:
-                l_fitpar_tp = np.array(p.map(fit_pulse_shape, pulse_tp[1, :, :]))
+                l_fitpar_tp = np.array(
+                    p.map(fit_pulse_shape, pulse_tp[1, :, :]))
 
             fitpar_tp = np.array([p_fitpar_tp, l_fitpar_tp])
 
         print('CREATE DATASET WITH TESTPULSES.')
         testpulses = h5f.create_group('testpulses')
         testpulses.create_dataset('event', data=np.array(pulse_tp))
-        testpulses.create_dataset('hours', data=np.array(metainfo_tp[0, :, 10]))
-        testpulses.create_dataset('testpulseamplitute', data=np.array(metainfo_tp[0, :, 12]))
+        testpulses.create_dataset(
+            'hours', data=np.array(metainfo_tp[0, :, 10]))
+        testpulses.create_dataset(
+            'testpulseamplitute', data=np.array(metainfo_tp[0, :, 12]))
 
         if calc_fit:
             testpulses.create_dataset('fitpar', data=np.array(fitpar_tp))
