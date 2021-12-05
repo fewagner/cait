432c7816651cf29363fdb33f341a933866ad4546
Felix Wagner <40992404+fewagner@users.noreply.github.com>
2021-12-05 19:09:15 +0100

    array fit exception

--- cait/fit/_numerical_fit.py
+++ cait/fit/_numerical_fit.py
@@ -22,92 +22,6 @@ def template(height, onset, offset, linear, quadratic, cubic, t, sev, timebase_m
                           max_shift=max_shift) + cubic * t ** 3 + quadratic * t ** 2 + linear * t + offset  # two times bounds samples shorter
 
 
-#
-# @nb.njit
-# def fitfunc(pars, event, t, sev, timebase_ms, max_shift, truncation_flag, truncated=False, t0=None, offset=None,
-#             linear=None, quadratic=None, cubic=None, saturation_pars=None):
-#     """TODO"""
-#     # event and t same length
-#     if t0 is not None:
-#         pars[1] = t0
-#     if offset is not None:
-#         pars[2] = offset
-#     if linear is not None:
-#         pars[3] = linear
-#     if quadratic is not None:
-#         pars[4] = quadratic
-#     if cubic is not None:
-#         pars[5] = cubic
-#     if np.abs(pars[1]) > max_shift:
-#         return 1e8 + 1e5*(np.abs(pars[1] - max_shift))
-#     if np.abs(pars[0]) > 100:
-#         return 1e5 * (np.abs(pars[0]))
-#     height = np.max(event)
-#     if height > 3 * np.std(event[:500]) and pars[0] < 0:
-#         return 1e5*(np.abs(pars[0]))
-#     if truncated:
-#         if pars[0] < height:
-#             return 1e8 + 1e4 * (height - pars[0])/height
-#     bound_samples = int(max_shift / timebase_ms)
-#     if saturation_pars is not None:
-#         sev[:] = scaled_logistic_curve(sev, saturation_pars[0], saturation_pars[1], saturation_pars[2],
-#                                     saturation_pars[3], saturation_pars[4], saturation_pars[5], )
-#     temp = template(height=pars[0], onset=pars[1], offset=pars[2], linear=pars[3], quadratic=pars[4], cubic=pars[5],
-#                     t=t, sev=sev, timebase_ms=timebase_ms, max_shift=max_shift)
-#     retval = np.mean(np.abs(temp[truncation_flag[bound_samples:-bound_samples]] -
-#                           event[bound_samples:-bound_samples][truncation_flag[bound_samples:-bound_samples]]) ** 2)
-#     return retval
-#
-#
-# def array_fitter(args, sev, t, record_length=16384, timebase_ms=0.04, max_shift=20,
-#                  truncation_level=None, saturation_pars=None):
-#     """TODO"""
-#     ev, ph_start, t0, t0_start = args
-#     bound_samples = int(max_shift/timebase_ms)
-#
-#     # set sv
-#     start_vals = np.array([ph_start, t0_start, 0, 0, 0, 0])
-#     start_vals[3], start_vals[2], _, _, _ = linregress(t[bound_samples:int(3 / 16 * record_length)],
-#                                                        ev[:int(3 / 16 * record_length) - bound_samples])
-#
-#     # truncation
-#     if truncation_level is not None:
-#         truncation_flag = ev < (truncation_level + start_vals[2])
-#     else:
-#         truncation_flag = np.ones(ev.shape[0], dtype=bool)
-#     if any(np.logical_not(truncation_flag)):
-#         truncated = True
-#     else:
-#         truncated = False
-#     if truncated:
-#         offset = start_vals[2]
-#         linear = 0
-#         quadratic = 0
-#         cubic = 0
-#     else:
-#         offset = None
-#         linear = None
-#         quadratic = None
-#         cubic = None
-#
-#     # minimize
-#     res = minimize(
-#         fun=fitfunc,
-#         x0=start_vals,
-#         method='Nelder-Mead',
-#         args=(ev, t[bound_samples:-bound_samples], sev,  # [bound_samples:-bound_samples],
-#               timebase_ms, max_shift, truncation_flag, truncated, t0, offset, linear, quadratic, cubic, saturation_pars),
-#         options={'maxiter': None, 'maxfev': None, 'xatol': 1e-8, 'fatol': 1e-8, 'adaptive': True},
-#     )
-#
-#     if truncated:
-#         res.x[2:6] = offset, linear, quadratic, cubic
-#     if t0 is not None:
-#         res.x[1] = t0
-#
-#     return res.x
-
-
 @nb.njit
 def doshift(arr, num, fill_value=np.nan):
     """TODO"""
@@ -133,7 +47,12 @@ def fitfunc(pars, X, event_, sev, bs):
     if np.abs(np.floor(shift)) > bs:
         return 1e8 + 1e5 * np.abs(shift - bs)
     X[:, 0] = doshift(sev, shift)[bs:-bs]
-    return np.mean((np.dot(X, lstsqsol(X, event_)) - event_) ** 2)
+    try:
+        rms = np.mean((np.dot(X, lstsqsol(X, event_)) - event_) ** 2)
+    except:
+        print('Error in array fit, filling up with zeros!')
+        rms = 0
+    return rms
 
 
 @nb.njit
@@ -175,7 +94,11 @@ def array_fit(args, sev, t, blcomp, trunclv, bs, no_bl_when_sat):
         else:
             res = t0
         A[:, 0] = doshift(sev, res)[bs:-bs]
-        x = lstsqsol(A, event_)
+        try:
+            x = lstsqsol(A, event_)
+        except:
+            print('Error in array fit, filling up with zeros!')
+            x = np.zeros(A.shape[1])
     else:
         if no_bl_when_sat:
             A = A[:, :2]
@@ -190,7 +113,11 @@ def array_fit(args, sev, t, blcomp, trunclv, bs, no_bl_when_sat):
         else:
             res = t0
         A_[:, 0] = doshift(sev, res)[bs:-bs][truncflag_]
-        x = lstsqsol(A_, event_[truncflag_])
+        try:
+            x = lstsqsol(A_, event_[truncflag_])
+        except:
+            print('Error in array fit, filling up with zeros!')
+            x = np.zeros(A_.shape[1])
 
     pars[0] = x[0]
     pars[1] = res * (t[1] - t[0])
