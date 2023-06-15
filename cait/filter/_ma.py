bug fixes in moving average and bin triggering

cait/filter/_ma.py
@@ -9,12 +9,12 @@ def box_car_smoothing(event, length=50):
     """
     Calculates a moving average on an event array and returns the smoothed event
 
-    :param event: 1D array, the event to calcualte the MA
+    :param event: 1D array, the event to calculate the MA
     :param length: the length of the moving average
     :return: 1D array the smoothed array
     """
     event = np.pad(event, length, 'edge')
-    event = 0.02 * np.convolve(event, np.array([1]).repeat(50), 'same')
+    event = 1/length * np.convolve(event, np.array([1]).repeat(length), 'same')
     return event[length:-length]
 
 

cait/mixins/_data_handler_bin.py
@@ -7,7 +7,7 @@ import h5py
 from ..features._mp import calc_main_parameters
 from ..trigger._csmpl import trigger_csmpl, align_triggers, sample_to_time, \
     exclude_testpulses, get_starttime, get_test_stamps, get_offset
-from ..trigger._bin import get_record_window_vdaq, trigger_bin
+from ..trigger._bin import get_record_window_vdaq, trigger_bin, read_header
 from tqdm.auto import tqdm
 from ..fit._pm_fit import fit_pulse_shape
 from ..fit._templates import pulse_template
@@ -227,7 +227,12 @@ class BinMixin(object):
                                   shape=aligned_triggers.shape,
                                   dtype=np.int64)
 
-            file_start_s = np.fromfile(path, dtype=dtype, count=1, offset=header_size)['Time'][0]/1e9
+            try:
+                file_start_s = np.fromfile(path, dtype=dtype, count=1, offset=header_size)['Time'][0]/1e9
+            except:
+                print("Failed to read file_start_s from first datapoint. Attempting to read it from header ...")
+                header, *_ = read_header(path)
+                file_start_s = header["timestamp"]/1e9
 
             stream['trigger_time_s'][...] = np.array(
                 aligned_triggers + file_start_s, dtype='int32')
@@ -319,11 +324,11 @@ class BinMixin(object):
             # open write file
             write_events = h5f.require_group('events')
             if exclude_tp:
-                cond_dac = np.any([tp_hours[tpas[i] >= min_tpa[i]] for i in range(self.nmbr_channels)], axis=0)
+                cond_dac = np.any([tpas[i] >= min_tpa[i] for i in range(self.nmbr_channels)], axis=0)
                 cond_tps = np.all(
                     [np.logical_and(tpas[i] >= min_tpa[i], tpas[i] < min_cpa[i]) for i in range(self.nmbr_channels)],
                     axis=0)
-                cond_cps = np.any([tp_hours[tpas[i] >= min_cpa[i]] for i in range(self.nmbr_channels)], axis=0)
+                cond_cps = np.any([tpas[i] >= min_cpa[i] for i in range(self.nmbr_channels)], axis=0)
                 write_testpulses = h5f.require_group('testpulses')
                 if origin is None:
                     write_controlpulses = h5f.require_group('controlpulses')
@@ -577,7 +582,9 @@ class BinMixin(object):
             time.append(trig)
             heights.append(height)
 
-        file_start_s = np.fromfile(path, dtype=dtype, count=1, offset=header_size)['Time'][0] / 1e9
+        #file_start_s = np.fromfile(path, dtype=dtype, count=1, offset=header_size)['Time'][0] / 1e9
+        header, *_ = read_header(path)
+        file_start_s = header["timestamp"]/1e9
 
         self.include_test_stamps_vdaq(triggers=time,
                                      tpas=heights,