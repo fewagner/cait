eccbba07d392d23862a881581dbb5c017b81f3e1
Philipp Schreiner <109656219+philipp-schreiner@users.noreply.github.com>
2024-05-13 17:13:32 +0200

    add dcache classes docstring

--- cait/readers/binaryfile.py
+++ cait/readers/binaryfile.py
@@ -6,9 +6,45 @@ import numpy as np
 import h5py
 
 class BinaryFile:
+    """
+    A class that can be used to open binary files (e.g. rdt or stream files) which also supports reading from dcache.
+
+    :param path: The full path (including file extension) to the file of interest. If the path starts with 'dcap://' it is assumed to be a dcache path and reading therefrom is attempted.
+    :type path: str
+    :param dtype: The numpy (structured) dtype to use when interpreting the contents of the file.
+    :type dtype: np.dtype
+    :param offset: The offset (in bytes) for reading the file, defaults to 0.
+    :type offset: int, optional
+    :param count: The number of items (of size given by dtype) to read. If -1, the entire file is read, defaults to -1.
+    :type count: int, optional
+
+    **Example:**
+    ::
+        import numpy as np
+        from cait.readers import BinaryFile
+        from cait.versatile import Line
+
+        rdt_file = "path/to/file.rdt"
+        dtype = np.dtype([ ('detector_nmbr', 'i4'), ('coincide_pulses', 'i4'),
+                           ('trig_count', 'i4'), ('trig_delay', 'i4'),
+                           ('abs_time_s', 'i4'), ('abs_time_mus', 'i4'),
+                           ('delay_ch_tp', 'i4', (1,)), ('time_low', 'i4'),
+                           ('time_high', 'i4'), ('qcd_events', 'i4'),
+                           ('hours', 'f4'), ('dead_time', 'f4'),
+                           ('test_pulse_amplitude', 'f4'), ('dac_output', 'f4'),
+                           ('samples', 'i2', 16384),
+                          ])
+
+        with BinaryFile(rdt_file, dtype=dtype) as f:
+            first_event = np.array(f[0]["samples"])
+
+        Line(first_event)
+    """
     def __init__(self, path: str, dtype: np.dtype, offset: int = 0, count: int = -1):
+        # Distinguish between reading from dcache or local
         self._mode = "dcache" if path.startswith("dcap://") else "local"
         
+        # Check if environment variable is set correctly to use libpdcap.so
         if self._mode == "dcache" and "LD_PRELOAD" not in os.environ.keys():
             raise OSError("To read files from dcache, the environment variable 'LD_PRELOAD' has to be set.")
         if self._mode == "dcache" and not os.path.exists(os.environ["LD_PRELOAD"]):
@@ -37,11 +73,13 @@ class BinaryFile:
         self._isopen = False
 
     def __enter__(self):
+        # yes, I tried to use numpy.memmap (uses mmap internally)
+        # yes, I tried to use mmap directly (seems not to work in principle)
+        # yes, the HDF5 file approach is stupid
+        # yes, I am so over dcache
         if self._mode == "dcache":
-            # yes, I tried to use numpy.memmap (uses mmap internally)
-            # yes, I tried to use mmap directly (seems not to work in principle)
-            # yes, the HDF5 file approach is stupid
-            # yes, I am so over dcache
+            # Create h5 dummy file in memory (driver="core" does not create a file
+            # and backing_store=False prevents generating an output file once closed)
             self._source = h5py.File("proxy", "a", driver="core", backing_store=False)
             g = self._source.require_group("datagroup")
             g.create_dataset("dataset", 
@@ -49,18 +87,23 @@ class BinaryFile:
                              dtype=self._dtype, 
                              external=((self._path, self._offset, self._size*self._dtype.itemsize),))
             
+            # The datasets of h5py are quite similar to numpy arrays. In particular,
+            # they can be indexed identically. Therefore, returning the h5 dataset here
+            # and the numpy memmap else creates an identical data handling
             self._f = self._source["datagroup/dataset"]
             self._isopen = True
             return self._f
         
+        # See the numpy.memmap implementation for reference
+        # (https://github.com/numpy/numpy/blob/v1.26.0/numpy/core/memmap.py)
         else:
-            # See the numpy.memmap implementation 
-            # (https://github.com/numpy/numpy/blob/v1.26.0/numpy/core/memmap.py)
             start = self._offset - self._offset % mmap.ALLOCATIONGRANULARITY
             array_offset = self._offset - start
             length = int(self._offset + self._size*self._dtype.itemsize) - start
 
             with open(self._path, "rb") as f:
+                # create the mmap object separately so that it can be nicely closed
+                # (numpy memmap does not close the mmap object!)
                 self._source = mmap.mmap(fileno=f.fileno(),
                                          length=length,
                                          access=mmap.ACCESS_READ,
@@ -71,6 +114,8 @@ class BinaryFile:
                                          dtype=self._dtype,
                                          buffer=self._source,
                                          offset=array_offset)
+            # The array returned here is treated identical to the h5 dataset returned
+            # in case we are reading from dcache.
             return self._f
         
     def __exit__(self, typ, val, tb):
@@ -80,4 +125,8 @@ class BinaryFile:
 
     def __getitem__(self, val):
         with self if not self._isopen else nullcontext(self._f) as f:
+            # When entering the context, self.__enter__ returns f, which
+            # we made sure behaves the same when being indexed in both cases. 
+            # Hence, we can just forward the __getitem__ call to the
+            # (numpy or hdf5) array
             return f.__getitem__(val)
--- cait/readers/textfile.py
+++ cait/readers/textfile.py
@@ -2,14 +2,35 @@ import os
 from ctypes import cdll
 
 class TextFile:
+    """
+    A class that can be used to open text files (e.g. par files) which also supports reading from dcache.
+
+    :param path: The full path (including file extension) to the file of interest. If the path starts with 'dcap://' it is assumed to be a dcache path and reading therefrom is attempted.
+    :type path: str
+
+    **Example:**
+    ::
+        from cait.readers import TextFile
+
+        par_file = "path/to/file.par"
+
+        with TextFile(par_file) as f:
+            print(f.read())
+    """
     def __init__(self, path: str):
+        # Distinguish between reading from dcache or local
         self._mode = "dcache" if path.startswith("dcap://") else "local"
         
+        # Check if environment variable is set correctly to use libpdcap.so
         if self._mode == "dcache" and "LD_PRELOAD" not in os.environ.keys():
             raise OSError("To read files from dcache, the environment variable 'LD_PRELOAD' has to be set.")
         if self._mode == "dcache" and not os.path.exists(os.environ["LD_PRELOAD"]):
             raise FileNotFoundError(f"The dcache library does not exist at the specified path {os.environ['LD_PRELOAD']}")
         
+        # The differences between reading from dcache or not are:
+        # 1. dcache needs a raw string as file path as well as 2. an opener
+        # If we wrap these differences in arguments for later, we can just use open() and
+        # conveniently get all functionality like readlines() for both implementations
         if self._mode == "dcache":
             self._open_args = ( path if isinstance(path, bytes) else path.encode('utf-8'), "r" )
             self._open_kwargs = { "opener": lambda fname, flags: cdll.LoadLibrary(os.environ["LD_PRELOAD"]).dc_open(fname, 0) }
@@ -18,6 +39,7 @@ class TextFile:
             self._open_args, self._open_kwargs = ( path, "r" ), {}
 
     def __enter__(self):
+        # return the open file to be used in a context
         self._f = open(*self._open_args, **self._open_kwargs)
         return self._f
     
