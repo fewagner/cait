9ba8ff2cc0a09db15764f324c1fcd82c35ae9662
Philipp Schreiner <philipp.schreiner@cern.ch>
2024-06-24 18:19:57 +0200

    bug fix in cresst stream implementation and trigger_collection

--- cait/mixins/_data_handler_trigger_collection.py
+++ cait/mixins/_data_handler_trigger_collection.py
@@ -12,10 +12,9 @@ class TriggerCollectionMixin:
     def trigger_multiTES_type1(self,
                              fpath: List[str],
                              path_par: str,
-                             record_length: int,
                              sigma: Union[float, List[float]],
                              interval: Tuple[float],
-                             path_tp: List[str, str, List[int]] = None,
+                             path_tp: List[Union[str, List[int]]] = None,
                              reuse_triggers: bool = True,
                              copy_events: bool = False,
                              **kwargs
@@ -24,27 +23,35 @@ class TriggerCollectionMixin:
         doc strings will be here
         """
         stream_arg = fpath + [path_par] + (path_tp[:2] if path_tp is not None else [])
-        sigmas = [sigma]*len(fpath) if isinstance(sigma, float) else sigma
-        stream = vai.Stream(hardware="cresst", stream_arg)
+        sigmas = [sigma]*len(fpath) if isinstance(sigma, (int, float)) else sigma
+        stream = vai.Stream("cresst", stream_arg)
         
-        rec_window_coinc = (-stream.dt_us*record_length/4, stream.dt_us*record_length/4)
+        rec_window_coinc = (-stream.dt_us*self.record_length//4, stream.dt_us*self.record_length//4)
         trigger_ts, trigger_phs = [], []
         
         for i, (key, sigma) in enumerate(zip(stream.keys, sigmas)):
             if reuse_triggers and self.exists("triggers", f"trigger_ts_{key}") and self.exists("triggers", f"trigger_phs_{key}"):
-                trigger_ts.append(list(self.get("triggers", f"trigger_ts_{key}")))
-                phs.append(list(self.get("triggers", f"trigger_phs_{key}")))
+                ts = list(self.get("triggers", f"trigger_ts_{key}"))
+                ph = list(self.get("triggers", f"trigger_phs_{key}"))
+                trigger_ts.append(ts)
+                trigger_phs.append(ph)
             else:
                 ind, ph = vai.trigger_zscore(stream[key],
-                                             record_length=record_length,
-                                             sigma=sigma,
+                                             record_length=self.record_length,
+                                             threshold=sigma,
                                              **kwargs)
                 ts = stream.time[ind]
     
-                self.set("triggers", f"trigger_ts_{key}"=np.array(ts, dtype=np.int64), f"trigger_phs_{key}"=np.array(ph), overwrite_existing=True)
+                self.set("triggers", 
+                         **{f"trigger_ts_{key}": np.array(ts)}, 
+                         dtype=np.int64, 
+                         overwrite_existing=True)
+                self.set("triggers", 
+                         **{f"trigger_phs_{key}": np.array(ph)}, 
+                         overwrite_existing=True)
                 
             if path_tp is not None:
-                tp_ts = stream.tp_timestamps[path_tp[2][i]]
+                tp_ts = stream.tp_timestamps[str(path_tp[2][i])]
                 *_, outside = vai.timestamp_coincidence(tp_ts, ts, rec_window_coinc)
                 ts = list(np.array(ts)[outside])
                 ph = list(np.array(ph)[outside])
@@ -58,7 +65,7 @@ class TriggerCollectionMixin:
             inside, coinc_inds, outside = vai.timestamp_coincidence(event_ts, ts, interval)
             event_ts = sorted(np.array(event_ts)[coinc_inds])
 
-        self.set("triggers", "timestamps"=np.array(event_ts, dtype=np.int64), overwrite_existing=True)
+        self.set("triggers", timestamps=np.array(event_ts), dtype=np.int64, overwrite_existing=True)
 
         
         
--- cait/versatile/datasources/stream/impl_cresst.py
+++ cait/versatile/datasources/stream/impl_cresst.py
@@ -52,7 +52,7 @@ class Stream_CRESST(StreamBaseClass):
                 mask = test_chs == k
                 self._tpas[str(k)] = tpas[mask]
                 # assuming 10 MHz clock
-                self._tp_timestamps[str(k)] = self.start_us + test_h[mask]*3600*1e6 + offset
+                self._tp_timestamps[str(k)] = self.start_us + np.array(test_h[mask]*3600*1e6, dtype=np.int64) + offset
 
         self._keys = list(self._data.keys())
 
