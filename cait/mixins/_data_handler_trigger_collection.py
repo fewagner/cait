e8f8ed4bb873dba2135abc66f8f0b7c5dd56732a
Samir Banik <samir.banik@oeaw.ac.at>
2024-08-01 17:48:16 +0200

    add trigger_coincidence first version (docstring incomplete)

--- cait/mixins/_data_handler_trigger_collection.py
+++ cait/mixins/_data_handler_trigger_collection.py
@@ -1,3 +1,5 @@
+import os
+
 from typing import List, Union, Tuple
 
 import numpy as np
@@ -9,65 +11,132 @@ class TriggerCollectionMixin:
     A mixin clas ....
     """
 
-    def trigger_multiTES_type1(self,
-                             fpath: List[str],
-                             path_par: str,
-                             sigma: Union[float, List[float]],
-                             interval: Tuple[float],
-                             path_tp: List[Union[str, List[int]]] = None,
-                             reuse_triggers: bool = True,
-                             copy_events: bool = False,
-                             **kwargs
-                             ):
-        """
-        doc strings will be here
-        """
-        stream_arg = fpath + [path_par] + (path_tp[:2] if path_tp is not None else [])
-        sigmas = [sigma]*len(fpath) if isinstance(sigma, (int, float)) else sigma
-        stream = vai.Stream("cresst", stream_arg)
-        
-        rec_window_coinc = (-stream.dt_us*self.record_length//4, stream.dt_us*self.record_length//4)
-        trigger_ts, trigger_phs = [], []
-        
-        for i, (key, sigma) in enumerate(zip(stream.keys, sigmas)):
-            if reuse_triggers and self.exists("triggers", f"trigger_ts_{key}") and self.exists("triggers", f"trigger_phs_{key}"):
-                ts = list(self.get("triggers", f"trigger_ts_{key}"))
-                ph = list(self.get("triggers", f"trigger_phs_{key}"))
+    def trigger_coincidence(self,
+                            filedict: dict,
+                            interval: Tuple[float] = None,
+                            sigma: Union[float, List[float]] = 5,
+                            reuse_triggers: bool = False,
+                            copy_events: bool = False,
+                            **kwargs
+                           ):
+            """
+            :param filedict: 
+            :type filedict: dict
+            :param interval: 
+            :type interval: Tuple[float], optional
+            :param sigma: 
+            :type sigma: Union[float, List[float]], optional
+            :param reuse_triggers: 
+            :type reuse_triggers: bool, optional
+            :param copy_events: 
+            :type copy_events: bool, optional
+            :param kwargs: Additional keyword arguments forwarded to ``cait.versatile.trigger_zscore``.
+            :type kwargs: Any
+            
+            **Example:**
+            ::
+                filedict = {
+                    "par": 'file.par',
+                    "tp": ['file.test_stamps', 'file.dig_stamps'],
+                    "ch4": ["file_ch4.csmpl", "5"],
+                    "ch5": ["file_ch5.csmpl", "5"],
+                    "ch6": ["file_ch6.csmpl", "7"],
+                }
+                
+                # perform triggering and save triggers in DataHandler
+                dh.trigger_coincidence(filedict, 
+                                       interval # microseconds
+                                       # n_triggers=1000 # example of how to only trigger the first 1000 events
+                                       )
+                                       
+                # Inspect coincidence histogram
+                vai.Histogram
+            """
+            stream_arg = [filedict.get("par")] + filedict.get("tp", []) + [v[0] for k,v in filedict.items() if k.lower().startswith("ch")]
+            tp_keys = {os.path.basename(os.path.splitext(v[0])[0]): v[1] for k,v in filedict.items() if k.lower().startswith("ch")}
+            stream = vai.Stream("cresst", stream_arg)
+            sigmas = [sigma]*len(stream.keys) if isinstance(sigma, (int, float)) else sigma
+
+            rec_window_coinc = (-stream.dt_us*self.record_length//4, stream.dt_us*self.record_length//4)
+            if interval is None: 
+                interval = rec_window_coinc
+
+            trigger_ts, trigger_phs = [], []
+
+            for i, (key, sigma) in enumerate(zip(stream.keys, sigmas)):
+                if reuse_triggers:
+                    if not (self.exists("triggers", f"trigger_ts_{key}") and self.exists("triggers", f"trigger_ph_{key}")):
+                        raise KeyError(f"To reuse triggers, datasets 'trigger_ts_{key}' and 'trigger_ph_{key}' must exist in the 'triggers' group.")
+
+                    ts = list(self.get("triggers", f"trigger_ts_{key}"))
+                    ph = list(self.get("triggers", f"trigger_ph_{key}"))
+
+                else:
+                    ind, ph = vai.trigger_zscore(stream[key],
+                                                 record_length=self.record_length,
+                                                 threshold=sigma,
+                                                 **kwargs)
+                    ts = stream.time[ind]
+
+                    # save trigger timestamps and trigger heights. Can be used in subsequent calls to avoid going through the trigger process again if just the interval argument for building events changes
+                    self.set("triggers", 
+                             **{f"trigger_ts_{key}": np.array(ts)}, 
+                             dtype=np.int64, 
+                             overwrite_existing=True)
+                    self.set("triggers", 
+                             **{f"trigger_ph_{key}": np.array(ph)}, 
+                             dtype=np.float32, 
+                             overwrite_existing=True)
+
+                # if testpulse information is provided, trigger timestamps within one record window around testpulse timestamps are counted as such
+                if "tp" in filedict.keys():
+                    tp_ts = stream.tp_timestamps[tp_keys[key]]
+                    *_, outside = vai.timestamp_coincidence(tp_ts, ts, rec_window_coinc)
+                    ts = list(np.array(ts)[outside])
+                    ph = list(np.array(ph)[outside])
+
                 trigger_ts.append(ts)
                 trigger_phs.append(ph)
-            else:
-                ind, ph = vai.trigger_zscore(stream[key],
-                                             record_length=self.record_length,
-                                             threshold=sigma,
-                                             **kwargs)
-                ts = stream.time[ind]
-    
-                self.set("triggers", 
-                         **{f"trigger_ts_{key}": np.array(ts)}, 
-                         dtype=np.int64, 
-                         overwrite_existing=True)
-                self.set("triggers", 
-                         **{f"trigger_phs_{key}": np.array(ph)}, 
-                         overwrite_existing=True)
+
+            # build events
+            event_ts = trigger_ts[0].copy()
+            coinc_flag = [[True]*len(event_ts)]
+            for i, ts in enumerate(trigger_ts[1:]):
+                inside, coinc_inds, outside = vai.timestamp_coincidence(event_ts, ts, interval)
+
+                for k in range(i+1):
+                    coinc_flag[k].extend([False]*len(outside))
+
+                new_flag = np.array([False]*len(event_ts) + [True]*len(outside))
+                new_flag[coinc_inds] = True
+                coinc_flag.append(new_flag)
+
+                new_ts = np.concatenate([event_ts, np.array(ts)[outside]])
+                sortind = np.argsort(new_ts)
+                event_ts = new_ts[sortind]
+
+                coinc_flag[i] = np.array(coinc_flag[i])[sortind].tolist()
+                coinc_flag[-1] = np.array(coinc_flag[-1])[sortind].tolist()
+
+            # save final timestamps after event building and coincidence information
+            self.set("triggers", timestamps=np.array(event_ts), dtype=np.int64, overwrite_existing=True)
+            self.set("triggers", coincidences=np.array(coinc_flag), dtype=bool, overwrite_existing=True)
+            
+            # also save the original trigger timestamps exactly like the coincidence array
+            original_ts = np.zeros(np.array(coinc_flag).shape, dtype=np.int64)
+            for i, t in enumerate(trigger_ts):
+                original_ts[i, np.array(coinc_flag[i])] = np.array(t)
                 
-            if path_tp is not None:
-                tp_ts = stream.tp_timestamps[str(path_tp[2][i])]
-                *_, outside = vai.timestamp_coincidence(tp_ts, ts, rec_window_coinc)
-                ts = list(np.array(ts)[outside])
-                ph = list(np.array(ph)[outside])
-                
-            trigger_ts.append(ts)
-            trigger_phs.append(ph)
+            self.set("triggers", original_timestamps=original_ts, dtype=np.int64, overwrite_existing=True)
 
-        # build events
-        event_ts = trigger_ts[0]
-        for ts in trigger_ts[1:]:
-            inside, coinc_inds, outside = vai.timestamp_coincidence(event_ts, ts, interval)
-            event_ts = sorted(np.array(event_ts)[coinc_inds])
+            if copy_events:
+                # save events in events group
+                self.include_event_iterator("events", stream.get_event_iterator(stream.keys, self.record_length, timestamps=event_ts))
 
-        self.set("triggers", timestamps=np.array(event_ts), dtype=np.int64, overwrite_existing=True)
-
-        
-        
-                                           
-                                           
+                # do the same for testpulses if respective information is provided
+                if "tp" in filedict.keys():
+                    # save testpulses and tpas in separate groups (as they might be pulsed differently)
+                    for key in stream.keys:
+                        group_name = f"testpulses_{key}"
+                        self.include_event_iterator(group_name, stream.get_event_iterator(key, self.record_length, timestamps=stream.tp_timestamps[tp_keys[key]]))
+                        self.set(group_name, testpulseamplitude=stream.tpas[tp_keys[key]])
