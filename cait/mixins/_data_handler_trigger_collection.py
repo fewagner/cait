1bad881b9efe40bca972dfb53400d56778b51a54
Philipp Schreiner <philipp.schreiner@cern.ch>
2024-08-02 14:41:44 +0200

    finish coincidence trigger (incomplete docs)

--- cait/mixins/_data_handler_trigger_collection.py
+++ cait/mixins/_data_handler_trigger_collection.py
@@ -20,6 +20,8 @@ class TriggerCollectionMixin:
                             **kwargs
                            ):
             """
+            Explain, what the meaning of the newly created datasets in the DataHandler is
+            
             :param filedict: 
             :type filedict: dict
             :param interval: 
@@ -43,14 +45,25 @@ class TriggerCollectionMixin:
                     "ch6": ["file_ch6.csmpl", "7"],
                 }
                 
-                # perform triggering and save triggers in DataHandler
-                dh.trigger_coincidence(filedict, 
-                                       interval # microseconds
+                # perform initial triggering and save triggers in DataHandler
+                # In this step, the events are built according to the record length of the DataHandler
+                dh.trigger_coincidence(filedict,
                                        # n_triggers=1000 # example of how to only trigger the first 1000 events
                                        )
                                        
-                # Inspect coincidence histogram
-                vai.Histogram
+                # Inspect coincidence histogram (coincidences of first two channels, here of a doubleTES module)
+                double_tes_flag = np.prod(dh.get("event_building", "trigger_flag", [0,1]), axis=0, dtype=bool)
+                ts = dh.get("event_building", "trigger_timestamps", [0,1])[:, double_tes_flag]
+                diffs = np.diff(ts, axis=0)
+                vai.Histogram(diffs/1000, bins=100, xlabel="delta (ms)")
+                
+                # Decide on a coincidence interval from the histogram, e.g. +- 5 ms
+                # Do the event building step again (note that raw triggers from previous call can be used)
+                dh.trigger_coincidence(filedict,
+                                       interval=(-5000, 5000), # microseconds
+                                       reuse_triggers=True,
+                                       # n_triggers=1000 # example of how to only trigger the first 1000 events
+                                       )
             """
             stream_arg = [filedict.get("par")] + filedict.get("tp", []) + [v[0] for k,v in filedict.items() if k.lower().startswith("ch")]
             tp_keys = {os.path.basename(os.path.splitext(v[0])[0]): v[1] for k,v in filedict.items() if k.lower().startswith("ch")}
@@ -100,34 +113,50 @@ class TriggerCollectionMixin:
 
             # build events
             event_ts = trigger_ts[0].copy()
-            coinc_flag = [[True]*len(event_ts)]
+            trigger_flag = [[True]*len(event_ts)]
+            
             for i, ts in enumerate(trigger_ts[1:]):
+                # determine coincidences
                 inside, coinc_inds, outside = vai.timestamp_coincidence(event_ts, ts, interval)
 
+                # all previous channels did not trigger for the newly found timestamps
+                # Therefore, we add False in the end of their trigger flag (will be sorted later)
                 for k in range(i+1):
-                    coinc_flag[k].extend([False]*len(outside))
+                    trigger_flag[k].extend([False]*len(outside))
 
+                # Build flag for the current channel. First initialize False in all existing spots.
+                # The new ones all get True because they are definitely triggered (by construction)
                 new_flag = np.array([False]*len(event_ts) + [True]*len(outside))
+                # Add True at the correct spots (where already existing events are)
                 new_flag[coinc_inds] = True
-                coinc_flag.append(new_flag)
+                trigger_flag.append(new_flag)
 
+                # Merge new timestamps with existing ones
                 new_ts = np.concatenate([event_ts, np.array(ts)[outside]])
+                # Get the sort indices (needed to sort timestamps AND the flags)
                 sortind = np.argsort(new_ts)
+                # Sort timestamps
                 event_ts = new_ts[sortind]
-
-                coinc_flag[i] = np.array(coinc_flag[i])[sortind].tolist()
-                coinc_flag[-1] = np.array(coinc_flag[-1])[sortind].tolist()
-
-            # save final timestamps after event building and coincidence information
-            self.set("triggers", timestamps=np.array(event_ts), dtype=np.int64, overwrite_existing=True)
-            self.set("triggers", coincidences=np.array(coinc_flag), dtype=bool, overwrite_existing=True)
-            
-            # also save the original trigger timestamps exactly like the coincidence array
-            original_ts = np.zeros(np.array(coinc_flag).shape, dtype=np.int64)
-            for i, t in enumerate(trigger_ts):
-                original_ts[i, np.array(coinc_flag[i])] = np.array(t)
                 
-            self.set("triggers", original_timestamps=original_ts, dtype=np.int64, overwrite_existing=True)
+                # Sort flag (IMPORTANT: has to be done for ALL previous lists!)
+                for j in range(i+2):
+                    trigger_flag[j] = np.array(trigger_flag[j])[sortind].tolist()
+
+            # save final timestamps and trigger flag after event building
+            self.set("event_building", event_timestamps=np.array(event_ts), dtype=np.int64, overwrite_existing=True)
+            self.set("event_building", trigger_flag=np.array(trigger_flag), dtype=bool, overwrite_existing=True)
+            
+            # also save the original trigger timestamps exactly like the trigger flag array
+            # values of -1 indicate that the corresponding value does not exist
+            # (because the channel didn't trigger separately for that event)
+            original_ts = -1*np.ones(np.array(trigger_flag).shape, dtype=np.int64)
+            original_ph = -1*np.ones(np.array(trigger_flag).shape, dtype=np.float32)
+            for i, (t, p) in enumerate(zip(trigger_ts, trigger_phs)):
+                original_ts[i, np.array(trigger_flag[i])] = np.array(t)
+                original_ph[i, np.array(trigger_flag[i])] = np.array(p)
+                
+            self.set("event_building", trigger_timestamps=original_ts, dtype=np.int64, overwrite_existing=True)
+            self.set("event_building", trigger_phs=original_ph, dtype=np.float32, overwrite_existing=True)
 
             if copy_events:
                 # save events in events group
