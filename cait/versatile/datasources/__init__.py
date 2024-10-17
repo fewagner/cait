6597b552f2343deeb5576cdc0a7cf742042cced3
Philipp Schreiner <109656219+philipp-schreiner@users.noreply.github.com>
2024-10-17 08:51:04 +0200

    import PARFile to vai

--- cait/versatile/datasources/__init__.py
+++ cait/versatile/datasources/__init__.py
@@ -1,4 +1,5 @@
 from .hardwaretriggered.rdt_file import RDTFile
+from .hardwaretriggered.par_file import PARFile
 from .stream.factory import Stream
 from .stream.impl_sum import StreamSum
 from .simulation.mock import MockData
