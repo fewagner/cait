172e9dcc620ef7fddbc933c86a23f962ce435a59
jtesarek <julius.tesarek@cern.ch>
2025-03-13 16:24:47 +0100

    little improvement

--- cait/versatile/eventfunctions/processing/fqlc.py
+++ cait/versatile/eventfunctions/processing/fqlc.py
@@ -41,21 +41,21 @@ class FQLC(FncBaseClass):
     :type thresh: float
     :param sat_v: The known true pulse height as defined by the saturation level. Necessary for method "satv".
     :type sat_v: float
-    :param known_fql_v: If the voltage drop of a FQL is known and provided here, the correction shift will be an integer multiple of this value, instead of any value. Defaults to None.
-    :type known_fql_v: float
+    :param known_fql_V: If the voltage drop of a FQL is known and provided here, the correction shift will be an integer multiple of this value, instead of any value. Defaults to None.
+    :type known_fql_V: float
     :param val_not_ev: If True, not the shifted event but the shift value is returned. Defaults to False.
     :type val_not_ev: bool
 
     :return: Event with FQL corrected or value of shift (if val_not_ev is set to True)
     :rtype: Union[numpy.ndarray, float]
     """
-    def __init__(self, method: str = "mmd", thresh: float = 0.5, sat_v: float = 4.00, known_fql_v: float=None, val_not_ev=False):
+    def __init__(self, method: str = "mmd", thresh: float = 0.5, sat_v: float = 4.00, known_fql_V: float=None, val_not_ev=False):
         self._remove_baseline = RemoveBaseline_new()
         self._mp = CalcMP()
         self._method = method
         self._thresh = thresh
         self._sat_v = sat_v
-        self._known_fql_v = known_fql_v
+        self._known_fql_V = known_fql_V
         self._val_not_ev = val_not_ev
 
     def __call__(self, event):
@@ -63,7 +63,7 @@ class FQLC(FncBaseClass):
         self._event_nobl = self._remove_baseline(event)
 
         if self._method == "mmd":
-            self._tmin1 = 600 + np.argmin(self._event_nobl[600:int(self._t_max)]) #start at 220 so we don't end up out of bounds when averaging later
+            self._tmin1 = 600 + np.argmin(self._event_nobl[600:int(self._t_max)]) #start at 600 so we don't end up out of bounds when averaging later
             self._tmin2 = max(600,int(self._t_max)) + np.argmin(self._event_nobl[max(600,int(self._t_max)):])
             #here, the 600 catches the case of a faulty or non-event with tmax before sample #600
             # take the values for the average not around the minima as the minima are subject to fluctuations.
@@ -80,28 +80,32 @@ class FQLC(FncBaseClass):
             print("Choose method from \"mmd\" (minmindiff), \"slope\" or \"satv\"")
             flux_loss = self._blavg1-self._blavg2 #minmindiff approach
         
+
+
         self._shifted_event = self._event_nobl.copy()
+        
+        if self._known_fql_V is not None: # FQL voltage is known and provided -> correct only for integer multiples
+            flux_loss = self._known_fql_V * np.ceil(flux_loss/self._known_fql_V - self._thresh)
+        
+        if self._val_not_ev:
+            return flux_loss
+            
         if flux_loss >= self._thresh: #only correct actual fql, not baseline drifts or the like
-            if self._known_fql_v is not None: # known fql voltage is provided -> only correct in integer multiples of it.
-                flux_loss = np.ceil(flux_loss/self._known_fql_v)*self._known_fql_v
             self._mp = CalcMP(box_car_smoothing={'length': 1}) # recalculate t0 without smoothing to be more precise
             _, self._t0, _, _, _, _, _, _, _ = self._mp(event) 
             self._shifted_event[int(self._t0)+1:] += flux_loss
-        else:
-            flux_loss = 0
-
-        if self._val_not_ev:
-            return flux_loss
 
         return self._shifted_event
         
     def preview(self, event):
         self(event)
+        t = (np.arange(len(event))-len(event)/4)*0.04 # samples to ms
 
-        d = {'event': [None, self._event_nobl],
-             'shifted event': [None, self._shifted_event]}
-    
-        return dict(line = d)
+        d = {'Event': [t, self._event_nobl],
+             'FQL corrected event': [t, self._shifted_event]}
+        ax = {"xaxis": {"label": "Time [ms]"}, "yaxis": {"label": "Voltage [V]"}}
+
+        return dict(axes=ax, line = d)
         
     def batch_support(self):
         return 'none'
