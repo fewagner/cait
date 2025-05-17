83bbd7c6f854ae1c1305a8ac9f34ba88e1841f9b
jtesarek <julius.tesarek@cern.ch>
2025-05-17 14:15:15 +0200

    Updated '__init__.py' with new event functions.

--- cait/versatile/eventfunctions/__init__.py
+++ cait/versatile/eventfunctions/__init__.py
@@ -4,10 +4,13 @@ from .processing.downsample import Downsample
 from .processing.optimumfiltering import OptimumFiltering
 from .processing.removebaseline import RemoveBaseline
 from .processing.tukey import TukeyWindow
-from .processing.fqlc import RemoveBaseline_new
-from .processing.fqlc import FQLC
+from .processing.fluxquantumlosscorrection import RemoveBaseline_VoltageMinimum
+from .processing.fluxquantumlosscorrection import FluxQuantumLossCorrection
 
 from .scalarfunctions.calcmp import CalcMP
 from .scalarfunctions.fitbaseline import FitBaseline
 from .scalarfunctions.templatefit import TemplateFit
 from .scalarfunctions.npeaks import NPeaks
+from .scalarfunctions.saturationtime import SaturationTime
+from .scalarfunctions.templatefit_fqlc import All_Peak_Positions
+from .scalarfunctions.templatefit_fqlc import TemplateFit_FQLC
