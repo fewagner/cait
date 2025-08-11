a9fb915d460dd0c6a6cf13d65286ae2e70555d11
Philipp Schreiner <109656219+philipp-schreiner@users.noreply.github.com>
2025-08-11 16:43:27 +0200

    feat (ecal): Implement new cait.versatile energy calibration interface with modular insertion of different testpulse responses and transfer functions.
    Implemented abstract classes 'vai.TestpulseResponse' and 'vai.TransferFunction' and polynomial as well as spline interpolation implementations. More might follow.
    Implemented a vai.EnergyCalibration object which combines the TestpulseResponse and TransferFunction objects to perform energy calibration both in the forward, as well as the inverse direction (i.e. TPE->PH and PH->TPE). Also implemented an interactive widget to test the workings of the calibration in vai.EnergyCalibration.preview()

--- cait/versatile/analysisobjects/__init__.py
+++ cait/versatile/analysisobjects/__init__.py
@@ -1,3 +1,6 @@
-from .sev import SEV
+from .ecal import EnergyCalibration
 from .nps import NPS
 from .of import OF
+from .sev import SEV
+from .testpulseresponse import TPRCubicSpline, TPRPoly
+from .transferfunction import TFPchip, TFPoly
