3908024c418a044b5c5a3da2bfc8efa8c90accde
Philipp Schreiner <109656219+philipp-schreiner@users.noreply.github.com>
2025-09-01 22:12:28 +0200

    docs(TriggerSurvival): Add example+plot to docstring.

--- cait/versatile/eventfunctions/scalarfunctions/triggersurvival.py
+++ cait/versatile/eventfunctions/scalarfunctions/triggersurvival.py
@@ -14,6 +14,8 @@ class TriggerSurvival(FncBaseClass):
     """
     Function that checks whether or not a given event would have survived triggering.
 
+    In the preview, the 'search_window'-box marks the area which is searched for samples exceeding the threshold. Parts outside the box are not searched because they can in general not be filtered correctly (c. f. :func:`cait.versatile.functions.trigger.triggerbase.trigger_base`). However, if a sample is found above threshold close to the right edge of the box, the maximum search is continued outside. 
+
     :param trigger_fnc: The trigger function to use. Has to have function signature ``f(event: np.ndarray) -> (trigger_inds: list, trigger_vals: list)``.
     :type trigger_fnc: callable
     :param target_ind: The index on the voltage trace where the event (maximum) was placed, i.e. where the trigger is expected to be found.
@@ -23,6 +25,52 @@ class TriggerSurvival(FncBaseClass):
 
     :return: Tuple ``(did_trigger, trigger_value, trigger_index)``. If no trigger was found (including tolerance), the tuple ``(False, 0, 0)`` is returned.
     :rtype: Tuple[bool, float, int]
+
+    .. code-block:: python
+
+        from functools import partial
+        import scipy as sp
+        import numpy as np
+
+        import cait.versatile as vai
+
+        # This example is not very meaningful. Usually, one would
+        # superimpose pulses onto empty baselines and check if they
+        # are triggered. To make this example self-contained, we
+        # use the pulses ov MockData (not empty baselines).
+        md = vai.MockData()
+        rl = md.record_length
+        sev, of = md.sev[0], md.of[0]
+        it = vai.MockData(record_length=6*rl).get_event_iterator()[0]
+
+        # Make a 'long' version of the SEV to be superimposed
+        padded_sev = np.zeros(6*rl)
+        padded_sev[3*rl:4*rl] = np.array(sev)
+
+        # Simulate random pulse heights and superimpose traces
+        sim_phs = sp.stats.uniform.rvs(size=len(it))
+        chunk_iterator = vai.iterators.PulseSimIterator(
+                it,
+                sev=padded_sev,
+                pulse_heights=sim_phs,
+        )
+
+        # Trigger function is trigger_of with a given threshold.
+        # If a trigger is found within 10 samples around target_ind 
+        # (the maximum of the SEV), the trigger is considered a 'survivor'
+        f = vai.TriggerSurvival(
+                trigger_fnc=partial(vai.trigger_of, of=of, threshold=0.1),
+                target_ind=np.argmax(padded_sev),
+                tolerance_samples=10
+            )
+
+        # Preview
+        vai.Preview(chunk_iterator, f)
+
+        # Calculate survival for all pulses
+        trigger_flag, trigger_val, trigger_ind = vai.apply(f, chunk_iterator)
+
+    .. image:: media/TriggerSurvival_preview.png
     """
     def __init__(self,
                  trigger_fnc: callable,
@@ -48,7 +96,7 @@ class TriggerSurvival(FncBaseClass):
         return 'none'
 
     def preview(self, event: np.ndarray) -> dict:
-        survived = self(event)
+        self(event)
         event = event - np.mean(event)
         x = np.arange(len(event))
         mine, maxe = np.min(event), np.max(event)
@@ -71,13 +119,16 @@ class TriggerSurvival(FncBaseClass):
                     filtered_event = vai.functions.trigger.trigger_of.filter_chunk(event, of, rl)
                     x_filtered = x[rl:-rl]
 
-                    l = l | {
+                    l = {**l, **{
                         "filtered event": [x_filtered, filtered_event],
                         "search_window": [
-                            [rl]*2+[None]+[N-2*rl]*2+[None]+[N-rl]*2,
-                            [mine, maxe, None, mine, maxe, None, mine, maxe]],
-                        "threshold": [ [rl, N-rl], [threshold]*2 ],
-                    }
+                            [rl, rl, N-2*rl, N-2*rl, rl],
+                            [mine, maxe, maxe, mine, mine]
+                        ],
+                            #[rl]*2+[None]+[N-2*rl]*2+[None]+[N-rl]*2,
+                            #[mine, maxe, None, mine, maxe, None, mine, maxe]],
+                        "threshold": [ [rl, N-2*rl], [threshold]*2 ],
+                    }}
             if is_same_function(self._f.func, vai.functions.trigger.trigger_zscore.trigger_zscore):
                 if all([kw in self._f.keywords.keys() for kw in ["record_length", "threshold"]]):
                     rl = self._f.keywords["record_length"]
@@ -90,12 +141,13 @@ class TriggerSurvival(FncBaseClass):
                     mine, maxe = np.min(filtered_event), np.max(filtered_event)
                     
                     l["target index"] = [ [self._ind]*2, [mine, maxe] ]
-                    l = l | {
+                    l = {**l, **{
                         "filtered event": [x_filtered, filtered_event],
                         "search_window": [
-                            [rl]*2+[None]+[N-2*rl]*2+[None]+[N-rl]*2,
-                            [mine, maxe, None, mine, maxe, None, mine, maxe]],
-                        "threshold": [ [rl, N-rl], [threshold]*2 ],
-                    }
+                            [rl, rl, N-2*rl, N-2*rl, rl],
+                            [mine, maxe, maxe, mine, mine]
+                        ],
+                        "threshold": [ [rl, N-2*rl], [threshold]*2 ],
+                    }}
 
         return dict(line=l, scatter=s)
