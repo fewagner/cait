af809eb6cf172b29b6204de13d211c9e8f21dbba
Colin Moore <moore@mpp.mpg.de>
2025-09-03 13:23:00 +0200

    fix: improper shape of timing parameters in preview()
    Only affects single-channel previews

--- cait/versatile/eventfunctions/scalarfunctions/mainparameters.py
+++ cait/versatile/eventfunctions/scalarfunctions/mainparameters.py
@@ -314,7 +314,7 @@ class MainParameters(FncBaseClass):
     def preview(self, event) -> dict:
         unsmoothed = event.copy()
         _ = self(event)
-        mp = np.array([self._peak_pos, self._os, self._rs, self._re, self._ds, self._de])
+        mp = np.array([self._peak_pos, self._os, self._rs, self._re, self._ds, self._de]).squeeze()
 
         _dt = self._dt_us if self._dt_us is not None else 1
         x = np.arange(event.shape[-1]) * _dt
