7ff86cc37afcc03b88c914b7afd122b8646a124a
Colin Moore <moore@mpp.mpg.de>
2025-12-12 12:15:53 +0100

    bug: Change underscore to dash to separate tag

--- cait/mixins/_data_handler_mainparameters.py
+++ cait/mixins/_data_handler_mainparameters.py
@@ -75,7 +75,7 @@ class MainParametersMixin:
         for n, t, d in zip(mp.names, mp.types, out):
             self.set(
                     group,
-                    **{f"{n}" + (f"_{tag}" if tag else ""): np.atleast_2d(d.T)},
+                    **{f"{n}" + (f"-{tag}" if tag else ""): np.atleast_2d(d.T)},
                     dtype=t,
                     overwrite_existing=True,
                     write_to_virtual=False,
