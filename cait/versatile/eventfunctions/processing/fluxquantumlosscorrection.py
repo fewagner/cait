b057cc5267d45ce2a5553ee9e13b42fa530bb8ab
Colin Moore <moore@mpp.mpg.de>
2025-12-12 13:26:46 +0100

    bug: remove debug print statement

--- cait/versatile/eventfunctions/processing/fluxquantumlosscorrection.py
+++ cait/versatile/eventfunctions/processing/fluxquantumlosscorrection.py
@@ -164,7 +164,6 @@ class FluxQuantumLossCorrection(FncBaseClass):
                 return self._flux_loss[ic]
 
             if self._flux_loss[ic] >= self._thresh[ic]: # only correct actual fql, not baseline drifts or the like
-                print("!")
                 mp = MainParameters(bcs={'length': 1}, fbl=dict(model=0, where=1/8)) # recalculate onset without smoothing to be more precise
                 self._t0[ic] = mp(event[ic])[2]
                 self._t0[ic] += event.shape[-1] // 4
