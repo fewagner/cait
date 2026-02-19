207b55afe8e4109a10e004236f2bf2ea6aa2d6cc
Colin Moore <moore@mpp.mpg.de>
2026-02-19 12:15:40 +0100

    bug: Change location of FQLC application from onset to min_deriv_index
    The previous FluxQuantumLossCorrection used the onset as the location to
    apply the correction, however this is often not well-defined when there
    is an FQL.  Changing this to min_deriv_index from MainParameters appears
    to work better in certain cases.

--- cait/versatile/eventfunctions/processing/fluxquantumlosscorrection.py
+++ cait/versatile/eventfunctions/processing/fluxquantumlosscorrection.py
@@ -113,8 +113,9 @@ class FluxQuantumLossCorrection(FncBaseClass):
         for ic in range(event.shape[0]):
             # Calculate main parameters
             mp = np.array(self._mp(event[ic]))
-            self._ph[ic], self._t0[ic], self._t_max[ic], self._lin_drift[ic] = \
-                    mp[np.array([0, 2, 1, 6])]
+            self._t0[ic] = min(mp[10], mp[12])
+            self._ph[ic], self._t_max[ic], self._lin_drift[ic] = \
+                    mp[np.array([0, 1, 6])]
             # The old t_end was relative to the record window, this is lost in the
             # new MainParameters, but the position relative to the peak position should
             # be close enough
@@ -164,9 +165,8 @@ class FluxQuantumLossCorrection(FncBaseClass):
                 return self._flux_loss[ic]
 
             if self._flux_loss[ic] >= self._thresh[ic]: # only correct actual fql, not baseline drifts or the like
-                mp = MainParameters(bcs={'length': 1}, fbl=dict(model=0, where=1/8)) # recalculate onset without smoothing to be more precise
-                self._t0[ic] = mp(event[ic])[2]
-                self._t0[ic] += event.shape[-1] // 4
+                mp = MainParameters(bcs={'length': 1}, fbl=dict(model=0, where=1/8))(event[ic]) # recalculate onset without smoothing to be more precise
+                self._t0[ic] = min(mp[10], mp[12])
                 self._corrected_event[ic, int(self._t0[ic])+1:] += self._flux_loss[ic]
 
         if was_1d:
