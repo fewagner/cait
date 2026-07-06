20184f499b706953801c1b7566021576a8f48d71
Philipp Schreiner <109656219+philipp-schreiner@users.noreply.github.com>
2026-07-06 17:29:46 +0200

    test: add tutorial tests

--- tests/test_tutorials.py
+++ tests/test_tutorials.py
@@ -3,19 +3,24 @@ import os
 import pytest
 
 from ._helper import cd, execute_notebook, get_absolute_path
-from .fixtures import tempdir
+from .fixtures import tempdir_fnc
 
 # THIS FILE RUNS ALL CELLS OF THE TUTORIAL NOTEBOOKS TO TEST FOR ERRORS
 
 @pytest.mark.parametrize("notebook", [
-    "tutorial_trigger",
+    "00tutorial_trigger",
+    "01datahandler_baseFeatures",
+    "02sev_nps_of_blres",
+    "03amplitude_reconstruction",
+    "04energy_calibration",
+    "05efficiency_simulation",
     # Add all tutorial notebooks that should run here:
     # Without file extension and relative to docs/source/tutorials/
     # NOTE: this runs the notebooks separately. I.e. variables, values, data
     # is not preserved between notebooks.
     ]
 )
-def test_tutorial(tempdir, notebook):
+def test_tutorial(tempdir_fnc, notebook):
     notebook_path = get_absolute_path(
         "..", 
         "docs", 
@@ -26,7 +31,7 @@ def test_tutorial(tempdir, notebook):
 
     # Make a separate folder for every notebook (same name as the notebook)
     # to prevent interaction between files created by the notebooks.
-    temp_dir = os.path.join(tempdir.name, notebook)
+    temp_dir = os.path.join(tempdir_fnc.name, notebook)
     os.mkdir(temp_dir)
 
     # Change to working directory of notebook and execute it.
